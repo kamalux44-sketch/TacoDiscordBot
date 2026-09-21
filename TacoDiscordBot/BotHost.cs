7cd5e6ed59cb0abf9cc698dcb58eb015c5c1b7cb
jise <jise@DESKTOP-2AP1KDI>
2026-09-22 08:34:36 +0900

    delete Texasポーカー

--- TacoDiscordBot/BotHost.cs
+++ TacoDiscordBot/BotHost.cs
@@ -33,8 +33,6 @@ public static class BotHost
 
     public static Services.PokerService PokerService { get; private set; }
 
-    public static Services.TexasPokerService TexasPokerService { get; private set; }
-
     public static Services.RouletteService RouletteService { get; private set; }
 
     public static Services.DoubleUpService DoubleUpService { get; private set; }
@@ -289,8 +287,6 @@ public static class BotHost
                 ? null
                 : new Services.PokerService(CoinService, pokerRepo);
 
-            TexasPokerService = new Services.TexasPokerService();
-
             RouletteService = CoinService == null
                 ? null
                 : new Services.RouletteService(CoinService, eventManager: EventManager);
@@ -337,9 +333,6 @@ public static class BotHost
 
             Client.ComponentInteractionCreated += Commands.PokerCommands.HandleComponentInteractionAsync;
             Client.ModalSubmitted += Commands.PokerCommands.HandleModalSubmitAsync;
-
-            Client.ComponentInteractionCreated += Commands.TexasPokerCommands.HandleComponentInteractionAsync;
-            Client.ModalSubmitted += Commands.TexasPokerCommands.HandleModalSubmitAsync;
 
             Client.ComponentInteractionCreated += Commands.RouletteCommands.HandleComponentInteractionAsync;
 
@@ -405,8 +398,6 @@ public static class BotHost
             slash.RegisterCommands<Commands.BlackjackCommands>();
 
             slash.RegisterCommands<Commands.PokerCommands>();
-
-            slash.RegisterCommands<Commands.TexasPokerCommands>();
 
             slash.RegisterCommands<Commands.RouletteCommands>();
 
